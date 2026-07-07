0e99987e21a11f2014fd4740585655bfbb3cd607
nikitaOsipof <nikita@ifmo.spb.ru>
2026-07-07 16:42:05 +0300

    Initial skeleton for data engineering course

---
+++ src/connector.py
@@ -0,0 +1,17 @@
+﻿class SQLiteDataConnector:
+    def __init__(self, db_path: str):
+        self.db_path = db_path
+        self.connection = None
+        self.cursor = None
+
+    def __enter__(self):
+        # TODO: Реализовать подключение к SQLite и создание курсора.
+        pass
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        # TODO: Реализовать логику закрытия ресурсов и rollback/commit.
+        pass
+
+    def insert_log(self, timestamp: str, level: str, message: str) -> None:
+        # TODO: Реализовать параметризованный SQL-запрос для вставки.
+        pass
---
+++ src/stream_reader.py
@@ -0,0 +1,8 @@
+﻿from typing import Generator, Dict
+
+def log_stream_reader(file_path: str) -> Generator[Dict[str, str], None, None]:
+    """
+    TODO: Реализуйте построчное чтение файла с помощью yield.
+    Фильтруйте только строки, содержащие 'ERROR'.
+    """
+    pass
