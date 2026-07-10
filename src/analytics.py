561f972affd1141b657cd11ea4c1330ba440907e
nikitaOsipof <nikita@ifmo.spb.ru>
2026-07-10 16:31:34 +0300

    Style: финальная фиксация форматирования заготовок кода

--- src/analytics.py
+++ src/analytics.py
@@ -1,2 +1,4 @@
 ﻿import polars as pl
+
+
 def calculate_category_metrics(source_path: str, target_path: str) -> None: pass
--- src/decorators.py
+++ src/decorators.py
@@ -1,6 +1,8 @@
 ﻿import time
 from functools import wraps
-from typing import Callable, Any
+from typing import Any, Callable
+
+
 def retry(max_tries: int = 3, delay: float = 2.0) -> Callable:
     def decorator(func: Callable) -> Callable:
         @wraps(func)
