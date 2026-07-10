fdcaeb42282fd8d5fc9da50d0ab12cdfc92c6850
nikitaOsipof <nikita@ifmo.spb.ru>
2026-07-10 16:29:20 +0300

    Quality: возвращена строгость CI/CD пайплайна и вычищены заготовки кода

--- src/connector.py
+++ src/connector.py
@@ -3,6 +3,12 @@
         self.db_path = db_path
         self.connection = None
         self.cursor = None
-    def __enter__(self): pass
-    def __exit__(self, exc_type, exc_val, exc_tb): pass
-    def insert_log(self, timestamp: str, level: str, message: str) -> None: pass
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+    def insert_log(self, timestamp: str, level: str, message: str) -> None:
+        pass
--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
-﻿import os
-import sys
-def run_pipeline() -> None: print("[Инфо] Пайплайн инициализирован.")
-if __name__ == "__main__": run_pipeline()
+﻿import sys
+
+
+def run_pipeline() -> None:
+    print("[Инфо] Пайплайн инициализирован.")
+
+if __name__ == "__main__":
+    run_pipeline()
